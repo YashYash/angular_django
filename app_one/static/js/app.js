82374327a637965c8803534bc0633db8aeb77539
YashYash <yash.saxena1217@gmail.com>
2014-07-03 01:16:11 -0700

    Facebook and email auth

--- app_one/static/js/app.js
+++ app_one/static/js/app.js
@@ -2,6 +2,11 @@ var buttons_app = angular.module('buttons_app', ['ngRoute','firebase']);
 
 buttons_app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
+        templateUrl: 'static/views/login.html',
+        controller: 'authController'
+    });
+
+    $routeProvider.when('/buttons', {
         templateUrl: 'static/views/buttons.html',
         controller: 'buttonsController'
     });
@@ -13,3 +18,20 @@ buttons_app.config(['$routeProvider', function ($routeProvider) {
 
 }]);
 
+buttons_app.directive('scrollOnClick', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, $elm, attrs) {
+      var idToScroll = attrs.href;
+      $elm.on('click', function() {
+        var $target;
+        if (idToScroll) {
+          $target = $(idToScroll);
+        } else {
+          $target = $elm;
+        }
+        $("body").animate({scrollTop: $target.offset().top}, "slow");
+      });
+    }
+  }
+});
