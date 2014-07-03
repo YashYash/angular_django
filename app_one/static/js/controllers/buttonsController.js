1019d10e2493aeb2779f854d4109783cf58a192b
Sahil Patel <spatel7@berkeley.edu>
2014-07-03 01:22:50 -0700

    Fixed shit

--- app_one/static/js/controllers/buttonsController.js
+++ app_one/static/js/controllers/buttonsController.js
@@ -3,11 +3,13 @@
  */
 function buttonsController($scope, $http, $routeParams, $firebase, $location) {
     var ref = new Firebase("https://buttonclicker.firebaseio.com/click-count");
-    console.log("controller working");
     ref.once('value', function(dataSnapshot) {
         refSnapshot = dataSnapshot;
         $scope.data = refSnapshot.val();
     });
+    if (!$scope.data) {
+        $scope.data = {};
+    }
     $scope.click_function = function(color){
         if ($scope.data[color]) {
             $scope.data[color]++;
@@ -15,6 +17,6 @@ function buttonsController($scope, $http, $routeParams, $firebase, $location) {
            $scope.data[color] = 1;
         }
         ref.set($scope.data);
-        window.location('/results');
+        $location.path('/results');
     }
 }
--- app_one/static/js/controllers/resultsController.js
+++ app_one/static/js/controllers/resultsController.js
@@ -1,20 +1,18 @@
 /**
- * Created by yash on 7/2/2014.
- */
-function buttonsController($scope, $http, $routeParams, $firebase) {
-    var ref = new Firebase("https://buttonclicker.firebaseio.com/click-count");
-    console.log("controller working");
-    ref.once('value', function(dataSnapshot) {
-        refSnapshot = dataSnapshot;
-        $scope.data = refSnapshot.val();
-    });
-    $scope.click_function = function(color){
-        if ($scope.data[color]) {
-            $scope.data[color]++;
-        } else {
-           $scope.data[color] = 1;
-        }
-        ref.set($scope.data);
-        console.log($scope.data[color]);
+* Created by yash on 7/2/2014.
+*/
+function resultsController($scope, $http, $routeParams, $firebase) {
+  var ref = new Firebase("https://buttonclicker.firebaseio.com/click-count");
+  console.log("controller working");
+  ref.once('value', function(dataSnapshot) {
+      refSnapshot = dataSnapshot;
+      $scope.data = refSnapshot.val();
+  });
+  if (!$scope.data) {
+    $scope.data = {
+      'blue': 0,
+      'red': 0,
+      'yellow': 0
     }
+  }
 }
